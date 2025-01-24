61bbf4095fea4c17c36259c828528197b70de655
IsaPirola <belapirola@gmail.com>
2025-01-24 18:19:52 -0300

    show seconds on tasks

--- src/utils/format-duration.ts
+++ src/utils/format-duration.ts
@@ -1,10 +1,12 @@
 export const formatDuration = (seconds: number): string => {
   const hours = Math.floor(seconds / 3600);
   const minutes = Math.floor((seconds % 3600) / 60);
+  const remainingSeconds = seconds % 60;
 
   // Formata para garantir dois dígitos
   const formattedHours = hours.toString().padStart(2, "0");
   const formattedMinutes = minutes.toString().padStart(2, "0");
+  const formattedSeconds = remainingSeconds.toString().padStart(2, "0");
 
-  return `${formattedHours}:${formattedMinutes}`;
+  return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
 };
