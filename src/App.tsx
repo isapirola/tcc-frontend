f24f3d017dceca81254e269ff76b9e97e59132f1
IsaPirola <belapirola@gmail.com>
2025-01-27 21:23:42 -0300

    vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import { Home, Login, Register } from "./pages";
 import { UserProvider } from "./context/UserContext";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path="/register" element={<Register />} />
         </Routes>
       </Router>
+      <Analytics />
     </UserProvider>
   );
 }
